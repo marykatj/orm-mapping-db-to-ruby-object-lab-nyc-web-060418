67fdab5203ad5b39db7476630b00a52c72a317bb
Mary-Kate James <marykatj@gmail.com>
2018-06-17 21:06:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,6 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    student_id = row[0]
-    student_name = row[1]
-    student_grade = row[2]
-    Student.new(student_id, student_name, student_grade)
     # create a new Student object given a row from the database
   end
 
@@ -18,16 +14,16 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
   end
-
+  
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade)
+      INSERT INTO students (name, grade) 
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-
+  
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
